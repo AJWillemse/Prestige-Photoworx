55dbc5541fba577a058ce1efdf92afb443d02578
Adriaan Willemse <129306390+AJWillemse@users.noreply.github.com>
2023-06-24 01:48:52 +0200

    Add files via upload

--- index.js
+++ index.js
@@ -42,3 +42,121 @@ window.addEventListener('load', function() {
   
   window.addEventListener('scroll', handleScroll);
   window.addEventListener('resize', handleScroll);
+
+
+
+
+
+document.addEventListener("DOMContentLoaded", function () {
+  
+  var previewImages = document.querySelectorAll("#imageContainer img");
+  var modalContainer = document.createElement("div");
+  var modalContent = document.createElement("div");
+  var modalImage = document.createElement("img");
+  var closeButton = document.createElement("span");
+  var body = document.body;
+  var photo = [
+      'images/image1.jpg',
+      'images/image2.jpg',
+      'images/image3.jpg',
+      'images/image4.jpg',
+      'images/image5.jpg',
+      'images/image6.jpg',
+      'images/image7.jpg',
+      'images/image8.jpg',
+      'images/image9.jpg',
+      'images/image10.jpg',
+      'images/image11.jpg',
+      'images/image12.jpg',
+      'images/image13.jpg',
+      'images/image14.jpg',
+      'images/image15.jpg',
+      'images/image16.jpg',
+      'images/image17.jpg',
+      'images/image18.jpg',
+      'images/image19.jpg',
+      'images/image20.jpg',
+      'images/image21.jpg',
+      'images/image22.jpg',
+      'images/image23.jpg',
+      'images/image24.jpg',
+      'images/image25.jpg',
+      'images/image26.jpg',
+      'images/image27.jpg',
+      'images/image28.jpg',
+      'images/image29.jpg',
+      'images/image30.jpg',
+      'images/image31.jpg',
+      'images/image32.jpg',
+      'images/image33.jpg',
+      'images/image34.jpg',
+      'images/image35.jpg',
+      'images/image36.jpg',
+      'images/image37.jpg',
+      'images/image38.jpg',
+      'images/image39.jpg',
+      'images/image40.jpg',
+      'images/image41.jpg',
+      'images/image42.jpg',
+      'images/image43.jpg',
+      'images/image44.jpg',
+      'images/image45.jpg',
+      'images/image46.jpg',
+      'images/image47.jpg',
+      'images/image48.jpg',
+    ];
+
+  modalContainer.id = "modalContainer";
+  modalContainer.classList.add("modal-container");
+  modalContainer.appendChild(modalContent);
+
+  
+  modalContent.id = "modalContent";
+  modalContent.classList.add("modal-content");
+  modalContent.appendChild(modalImage);
+  modalContent.appendChild(closeButton);
+
+
+  modalImage.id = "modalImage";
+  modalImage.classList.add("modal-image");
+  modalImage.setAttribute("alt", "");
+
+ 
+  closeButton.classList.add("close-button");
+  closeButton.innerHTML = "&times;";
+
+  
+  document.body.appendChild(modalContainer);
+
+  
+  previewImages.forEach(function (image) {
+    image.addEventListener("click", function () {
+      
+      var source = image.getAttribute("src");
+      var alt = image.getAttribute("alt");
+
+      
+      modalImage.setAttribute("src", source);
+      modalImage.setAttribute("alt", alt);
+
+     
+      modalContainer.style.display = "block";
+
+     
+      body.classList.add("overflow-hidden");
+    });
+  });
+
+  closeButton.addEventListener("click", function () {
+   
+    modalContainer.style.display = "none";
+
+    body.classList.remove("overflow-hidden");
+  });
+});
+  
+  
+  
+  
+  
+  
